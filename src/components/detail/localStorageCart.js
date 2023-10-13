a3b9981e1acc5e398e830cd279829b35ac1ede86
andres esteban ruiz <140468504+andresEstebanRuiz@users.noreply.github.comse>
2023-10-12 23:31:38 -0500

    comentario

--- src/components/detail/localStorageCart.js
+++ src/components/detail/localStorageCart.js
@@ -9,6 +9,24 @@ function localStorageCart(size,buttons,productId,name,price,image) {
   const carGet = localStorage.getItem('cart')
   const arrayCart=JSON.parse(carGet) || []
 
+  /*[
+    {size:'s', productId:1, name:'', price:'', image:'', quality:1}
+    {size:'m', productId:1, name:'', price:'', image:'', quality:2}
+    {size:'s', productId:18, name:'', price:'', image:'', quality:1}
+    {size:'l', productId:12, name:'', price:'', image:'', quality:1}
+  ]
+  */
+ const matched = arrayCart.find((product)=> product.productId === productId && product.zise === size)//me retorna el find el producto que cumpla con la condicion que estableci, o me devolvera undefined
+
+ if(matched){
+  //si el producto esta en el carrito aumentare la cantidad
+  matched.quantity++;
+ }else {
+  //si el producto no esta en el carro lo creare, Recordar que estoy discriminando por id y talla de ropa
+  arrayCart.push({size, productId, name, price,quantity:1 })
+ }
+ //({size:size, productId:productId, name:name, price:price,quantity:1 })
+
 
 // guardar el carrito actualizado en localStorage
 
